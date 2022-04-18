6c56ddf7f83b77d719f08c4ee5563b074aaa3a2c
FangWen-Hao <t108590048@ntut.org.tw>
2022-04-18 16:06:20 +0800

    Question 4-21 added

---
+++ Q4-21/Q4-21.c
@@ -0,0 +1,62 @@
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <limits.h>
+#include <pthread.h>
+#include <stdlib.h>
+
+#define BUFFER_SIZE 4096
+
+u_int32_t buffer[BUFFER_SIZE];
+
+void *getFibonacciNumber(void *param)
+{
+    int *p = param;
+    int numberOfFibonacciNumbers = *p;
+
+    buffer[0] = 0;
+    buffer[1] = 1;
+    for (int i = 2; i < numberOfFibonacciNumbers; i++)
+    {
+        buffer[i] = buffer[i - 1] + buffer[i - 2];
+    }
+
+    pthread_exit(0);
+}
+
+int main(int argc, char* argv[])
+{
+    pthread_t tid;
+    pthread_attr_t attr;
+    if (argc < 2) {
+        printf("No arguments given, program aborted.\n");
+        exit(1);
+    }
+
+    else if (argc > 2) {
+        printf("Too many arguments given, program aborted. \n");
+        exit(1);
+    }
+
+    int numberOfFibonacciNumbers = atoi(argv[1]);
+
+    if (numberOfFibonacciNumbers >= 48)
+    {
+        printf("The desired amounts of numbers, %d, will overflow, however the overflowed result will still be shown.\n", numberOfFibonacciNumbers);
+    }
+    // get thread attributes
+    pthread_attr_init(&attr);
+
+    // create a thread
+    pthread_create(&tid, &attr, getFibonacciNumber, &numberOfFibonacciNumbers);
+
+    // wait for each thread to finish
+    pthread_join(tid, NULL);
+
+    for (int i = 0; i < numberOfFibonacciNumbers; i++)
+    {
+        printf("%d ", buffer[i]);
+    }
+    printf("\n");
+}
